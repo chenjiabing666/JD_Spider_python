3a3294c7826aa0aa288500b92c776ca52a4391b7
yrong <yrong1997@gmail.com>
2017-05-21 21:00:42 +0800

    readme enhancement

--- SQL.py
+++ SQL.py
@@ -7,12 +7,12 @@ class save_mysql:
         self.user='root'
         self.password='root'
         self.host='localhost'
-        self.database='python'
+        self.database='jd_crawler'
     def get_connection(self):
-        return db.connect(user="root",passwd="root",host="localhost",db="python",charset="utf8")
+        return db.connect(user="root",passwd="root",host="127.0.0.1",port=3306,db="jd_crawler",charset="utf8")
 
     def save_img(self,url):
         conn=self.get_connection()
         cursor=conn.cursor()
-        cursor.execute("insert into JD(id,img_url) values(NULL,%s)",url)   #将img_url插入到数据库中
+        cursor.execute("insert into JD(img_url) values(%s)", [url])   #将img_url插入到数据库中
         conn.commit()
